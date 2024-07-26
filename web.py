7d803f299d5ff6efa2f4567cef9ad4c3f0ee92e5
zly3 <123818362+zly3@users.noreply.github.com>
2024-07-26 15:45:22 +0800

    Add files via upload

---
+++ web.py
@@ -0,0 +1,36 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('st.write')
+
+# 样例 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# 样例 2
+
+st.write(1234)
+
+# 样例 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# 样例 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# 样例 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+st.write(df2)
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
